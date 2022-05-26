28fb596da592650f832fee681a3363fe68fd15e0
Ak <akalankapathirage22@gmail.com>
2022-05-26 14:06:22 +0530

    Governance Token

---
+++ contracts/GovernanceToken.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.6.0 <0.9.0;
+
+// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol"; // By importing this above two are automatically added to the contract
+
+contract GovernanceToken is ERC20Votes {
+    uint256 public s_maxSupply = 1000000000000000000000000; // This is 1 million tokens with 18 decimals
+
+    constructor()
+        ERC20("GovernanceToken", "GOVT")
+        ERC20Permit("GovernanceToken")
+    {
+        _mint(msg.sender, s_maxSupply);
+    }
+
+    // The functions below are overrides required by Solidity.
+    function _afterTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20Votes) {
+        super._afterTokenTransfer(from, to, amount); //* This hook is applied when the token is transferred ( _mint, _burn, _transfer )
+                                                    //* Why is _transfer not overridden? This is from openzeppelin boilerplate.
+    }
+
+    function _mint(address to, uint256 amount) internal override(ERC20Votes) {
+        super._mint(to, amount);
+    }
+
+    function _burn(address account, uint256 amount)
+        internal
+        override(ERC20Votes)
+    {
+        super._burn(account, amount);
+    }
+}
