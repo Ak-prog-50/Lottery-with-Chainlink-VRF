281c2d662483d4b2960d04d357a5425e5230ba1d
Ak <akalankapathirage22@gmail.com>
2022-05-27 12:38:52 +0530

    deploy script to gtoken + ganache ui deploy

--- deploy/01_deploy_governer_token.ts
+++ deploy/01_deploy_governer_token.ts
@@ -1,17 +1,27 @@
-import {HardhatRuntimeEnvironment} from 'hardhat/types';
-import {DeployFunction} from 'hardhat-deploy/types';
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+import { ethers } from "hardhat";
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-  const {deployments, getNamedAccounts} = hre;
-  const {deploy} = deployments;
+  const { deployments, getNamedAccounts } = hre;
+  const { deploy, log } = deployments;
 
-  const {deployer} = await getNamedAccounts();
+  const { deployer } = await getNamedAccounts();
 
-  await deploy('GovernanceToken', {
+  const governanceToken = await deploy("GovernanceToken", {
     from: deployer,
     log: true,
   });
 
+  // await delegate(governanceToken.address, deployer)
 };
+
+const delegate = async (governanceTokenAddr: string, delegatedAcc: string) => {
+  const governanceToken = await ethers.getContractAt("GovernanceToken", governanceTokenAddr);
+  const tx = await governanceToken.delegate(delegatedAcc)
+  await tx.wait(1)
+  console.log(`Num of Checkpoints for ${delegatedAcc}: ${await governanceToken.numCheckpoints(delegatedAcc)}`)
+};
+
 export default func; //can use whatever name in here. Hardhat deploy will import the export as "func"
 func.tags = ["GToken"];
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -26,10 +26,8 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 const config: HardhatUserConfig = {
   solidity: "0.8.4",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    ganache: {
+      url: "HTTP://127.0.0.1:7545",
     },
   },
   gasReporter: {
