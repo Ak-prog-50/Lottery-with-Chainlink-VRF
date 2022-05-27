423ec276e95413a95a9125850ea22a022b4faa9c
Ak <akalankapathirage22@gmail.com>
2022-05-27 18:34:19 +0530

    deployLotter + helper-config

---
+++ deploy/06_deploy_lottery.ts
@@ -0,0 +1,63 @@
+import { networkConfig, developmentChains, VERIFICATION_BLOCK_CONFIRMATIONS } from "../helper-hardhat.config"
+import {HardhatRuntimeEnvironment} from 'hardhat/types';
+import { ethers, network } from "hardhat";
+import 'dotenv/config'
+import { DeployFunction } from "hardhat-deploy/types";
+import { BigNumber } from "ethers";
+// import { verify } from "../helper-functions"
+
+const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+    const { deployments, getNamedAccounts } = hre
+    const { deployer } = await getNamedAccounts()
+    const { deploy, log } = deployments
+
+    const chainId = hre.network.config.chainId
+    if (!chainId) return
+    const entranceFeeInUSD = 50
+    let subscriptionId:BigNumber
+    let vrfCoordinatorAddr: string | undefined
+    let priceFeedAddr: string | undefined
+
+    if (chainId === 31337) {
+        const priceFeed = await deployments.get("MockV3Aggregator")
+        const VRFCoordinatorV2MockDepl = await deployments.get("VRFCoordinatorV2Mock")
+        const VRFCoordinatorV2Mock = await ethers.getContractAt("VRFCoordinatorV2Mock", VRFCoordinatorV2MockDepl.address)
+        
+        const fundAmount = networkConfig[chainId].fundAmount
+        const transaction = await VRFCoordinatorV2Mock.createSubscription()
+        //* seems like addConsumer is not needed in mock testing
+        const transactionReceipt = await transaction.wait(1)
+        if (!transactionReceipt.events) return
+        subscriptionId = ethers.BigNumber.from(transactionReceipt.events[0].topics[1])
+        await VRFCoordinatorV2Mock.fundSubscription(subscriptionId, fundAmount)
+        priceFeedAddr = priceFeed.address
+        vrfCoordinatorAddr = VRFCoordinatorV2Mock.address
+    } else {
+        priceFeedAddr = networkConfig[chainId].ethUsdPriceFeed,
+        vrfCoordinatorAddr = networkConfig[chainId].vrfCoordinator
+        subscriptionId = BigNumber.from(process.env.VRF_SUBSCRIPTION_ID)
+    }
+
+    const waitBlockConfirmations = developmentChains.includes(network.name)
+    ? 1
+    : VERIFICATION_BLOCK_CONFIRMATIONS
+
+    console.log("\n\tSubscriptionId", subscriptionId, "\n")
+    const args = [priceFeedAddr, vrfCoordinatorAddr, entranceFeeInUSD, subscriptionId]
+
+    const lottery = await deploy("Lottery", {
+        from: deployer,
+        args: args,
+        log: true,
+        // waitConfirmations: waitBlockConfirmations
+    })
+
+    // if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+    //     log("Verifying...")
+    //     await verify(lottery.address, args)
+    //   }
+}
+
+module.exports = func
+func.tags = ["lottery"]
+module.exports.dependencies = ["mocks"]
--- helper-hardhat.config.ts
+++ helper-hardhat.config.ts
@@ -1,22 +1,39 @@
 export const MIN_DELAY = 100; //100 seconds
+import { BigNumber } from "ethers"
 
-const networkConfig = {
+type NetworkConfigItem = {
+  name: string
+  fundAmount: BigNumber
+  fee?: string
+  keyHash?: string
+  interval?: string
+  linkToken?: string
+  vrfCoordinator?: string
+  keepersUpdateInterval?: string
+  oracle?: string
+  jobId?: string
+  ethUsdPriceFeed?: string
+}
+
+type NetworkConfigMap = {
+  [chainId: string]: NetworkConfigItem
+}
+
+export const networkConfig: NetworkConfigMap = {
   default: {
     name: "hardhat",
     fee: "100000000000000000",
-    keyHash:
-      "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
+    keyHash: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
     jobId: "29fa9aa13bf1468788b7cc4a500a45b8",
-    fundAmount: "1000000000000000000",
+    fundAmount: BigNumber.from("1000000000000000000"),
     keepersUpdateInterval: "30",
   },
   31337: {
     name: "localhost",
     fee: "100000000000000000",
-    keyHash:
-      "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
+    keyHash: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
     jobId: "29fa9aa13bf1468788b7cc4a500a45b8",
-    fundAmount: "1000000000000000000",
+    fundAmount: BigNumber.from("1000000000000000000"),
     keepersUpdateInterval: "30",
     ethUsdPriceFeed: "0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419",
   },
@@ -27,32 +44,31 @@ const networkConfig = {
     oracle: "0xc57b33452b4f7bb189bb5afae9cc4aba1f7a4fd8",
     jobId: "d5270d1c311941d0b08bead21fea7747",
     fee: "100000000000000000",
-    fundAmount: "100000000000000000", // 0.1
+    fundAmount: BigNumber.from("1000000000000000000"),
     keepersUpdateInterval: "30",
   },
   4: {
     name: "rinkeby",
     linkToken: "0x01be23585060835e02b77ef475b0cc51aa1e0709",
     ethUsdPriceFeed: "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
-    keyHash:
-      "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
+    keyHash: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
     vrfCoordinator: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
     oracle: "0xc57b33452b4f7bb189bb5afae9cc4aba1f7a4fd8",
     jobId: "6b88e0402e5d415eb946e528b8e0c7ba",
     fee: "100000000000000000",
-    fundAmount: "100000000000000000", // 0.1
+    fundAmount: BigNumber.from("1000000000000000000"),
     keepersUpdateInterval: "30",
   },
   1: {
     name: "mainnet",
     linkToken: "0x514910771af9ca656af840dff83e8264ecf986ca",
-    fundAmount: "0",
+    fundAmount: BigNumber.from("0"),
     keepersUpdateInterval: "30",
   },
   5: {
     name: "goerli",
     linkToken: "0x326c977e6efc84e512bb9c30f76e30c160ed06fb",
-    fundAmount: "0",
+    fundAmount: BigNumber.from("0"),
   },
   137: {
     name: "polygon",
@@ -61,11 +77,9 @@ const networkConfig = {
     oracle: "0x0a31078cd57d23bf9e8e8f1ba78356ca2090569e",
     jobId: "12b86114fa9e46bab3ca436f88e1a912",
     fee: "100000000000000",
-    fundAmount: "100000000000000",
+    fundAmount: BigNumber.from("100000000000000"),
   },
-};
+}
 
-const developmentChains = ["hardhat", "localhost"];
-const VERIFICATION_BLOCK_CONFIRMATIONS = 6;
-
-export { networkConfig, developmentChains, VERIFICATION_BLOCK_CONFIRMATIONS };
+export const developmentChains: string[] = ["hardhat", "localhost"]
+export const VERIFICATION_BLOCK_CONFIRMATIONS = 6
