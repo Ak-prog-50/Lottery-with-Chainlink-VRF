31cea33f2b5d83aaa326f0d4010832044e624b03
Ak <akalankapathirage22@gmail.com>
2022-06-02 15:47:18 +0530

    GovernanceToken

--- contracts/GovernanceToken.sol
+++ contracts/GovernanceToken.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.6.0 <0.9.0;
 
-// import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-// import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol"; // By importing this above two are automatically added to the contract
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
-contract GovernanceToken is ERC20Votes {
+contract GovernanceToken is ERC20, ERC20Permit, ERC20Votes {
     uint256 public s_maxSupply = 1000000000000000000000000; // This is 1 million tokens with 18 decimals
 
     constructor()
@@ -20,18 +20,18 @@ contract GovernanceToken is ERC20Votes {
         address from,
         address to,
         uint256 amount
-    ) internal override(ERC20Votes) {
+    ) internal override(ERC20, ERC20Votes) {
         super._afterTokenTransfer(from, to, amount); //* This hook is applied when the token is transferred ( _mint, _burn, _transfer )
                                                     //* Why is _transfer not overridden? This is from openzeppelin boilerplate.
     }
 
-    function _mint(address to, uint256 amount) internal override(ERC20Votes) {
+    function _mint(address to, uint256 amount) internal override(ERC20, ERC20Votes) {
         super._mint(to, amount);
     }
 
     function _burn(address account, uint256 amount)
         internal
-        override(ERC20Votes)
+        override(ERC20, ERC20Votes)
     {
         super._burn(account, amount);
     }
