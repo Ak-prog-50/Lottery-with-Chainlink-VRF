c05d785a46f9c1fddde4fa9db508d7a2029abaef
Akalanka <81894220+Ak-prog-50@users.noreply.github.com>
2022-12-13 21:01:35 +0530

    entrance fee calculation + timestamps + 2 setters

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -21,7 +21,7 @@ contract Lottery is VRFConsumerBaseV2, Ownable {
         SELECTING_WINNER
     }
 
-    int8 public immutable i_entranceFeeInUsd;
+    int8 public s_entranceFeeInUsd; // 50 USD
     VRFCoordinatorV2Interface immutable i_vrfCoordinator;
 
     mapping(address => uint256) public s_addressToAmountDeposited;
@@ -30,10 +30,13 @@ contract Lottery is VRFConsumerBaseV2, Ownable {
     address payable public s_recentWinner;
     uint256 public s_requestId;
     uint256 public s_maxParticpantsLimit;
+    uint256 public s_lotteryEndTimestamp;
+    uint32 public s_lotteryDuration = 24 hours; // returns 86400 seconds
     uint64 s_subscriptionId;
     LotteryState public s_lotteryState;
     AggregatorV3Interface internal s_priceFeed;
 
+    // TODO: setters for these???
     bytes32 constant KEY_HASH =
         0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f; //* gas lane key hash (check docs for more info)
     uint32 constant CALLBACK_GAS_LIMIT = 100000; //* gas limit when VRF callback rawFulfillRandomWords func in VRFConsumerBaseV2.
@@ -50,7 +53,7 @@ contract Lottery is VRFConsumerBaseV2, Ownable {
         uint64 _subscriptionId,
         uint256 _maxParticpantsLimit
     ) VRFConsumerBaseV2(_vrfCoordinator) {
-        i_entranceFeeInUsd = _entranceFeeInUsd;
+        s_entranceFeeInUsd = _entranceFeeInUsd;
         s_priceFeed = AggregatorV3Interface(_priceFeed);
         s_lotteryState = LotteryState.CLOSED; //* default lottery state is closed
         s_subscriptionId = _subscriptionId;
@@ -65,12 +68,22 @@ contract Lottery is VRFConsumerBaseV2, Ownable {
         _;
     }
 
+    function setLotteryDuration(uint32 _durationInSecs) public onlyOwner {
+        s_lotteryDuration = _durationInSecs;
+    }
+
+    function setEntranceFee(int8 _entranceFeeInUsd) public onlyOwner {
+        s_entranceFeeInUsd = _entranceFeeInUsd;
+    }
+
     function startLottery() external onlyOwner {
         require(s_lotteryState == LotteryState.CLOSED);
+        s_lotteryEndTimestamp = block.timestamp + uint256(s_lotteryDuration);
         s_lotteryState = LotteryState.OPEN;
     }
 
     function endLottery() external onlyOwner checkOpened {
+        //TODO: check if the time is right???
         require(s_participants.length > 0, "No participants");
 
         s_lotteryState = LotteryState.SELECTING_WINNER;
@@ -85,19 +98,23 @@ contract Lottery is VRFConsumerBaseV2, Ownable {
     }
 
     function getEntranceFee() public view returns (uint256) {
-        (, int256 answer, , , ) = s_priceFeed.latestRoundData(); // * returns ETH/USD rate with 8 decimal places as answer
-        // console.log(uint(answer), "answer");
+        (, int256 price, , , ) = s_priceFeed.latestRoundData(); // * returns ETH/USD rate with 8 decimal places as answer
+        uint256 entranceFeeParsed = uint256(uint8(s_entranceFeeInUsd));
+        // entranceFeeParsed = 50; price = 1200;
+        // 1200 is the 1 eth representation in usd.
+        // Divide 50 by 1200 to get how much eth is in 50 usd.
 
-        int256 roundedAnswer = answer / (10**8); // * 245678999700 => rounded as 2456
-        // console.log(uint(roundedAnswer), "roundedAnswer");
+        // multiply 50 by 10^8 to match the 8 decimal places of the price.
+        // Technically, 50*10^8 / uint(price) is equal to 50 / 1200.
+        // And that's the amount of eth inside 50 usd. ( Cost to enter in eth = 0.04166666 )
+        // Then can convert that eth amount to wei by multiplying by 10^18.
 
-        int256 oneUSDInWei = 1 ether / roundedAnswer; // notes: answers decimals are ignored. need to recheck how to do rounding better
-        // console.log(uint(oneUSDInWei), "oneUsdInWEi");
+        // But due to solidity math limitations, we multiply 50 by 10^8 and 10^18 to get the correct result.
+        // Otherwise trying to divide (50 * 10^8) by (1200 * 10^8) will result in 0.
 
-        int256 entranceFeeInWei = oneUSDInWei * i_entranceFeeInUsd;
-        // console.log(uint(entranceFeeInWei), "entranceFeeInwei");
-
-        return uint256(entranceFeeInWei);
+        uint256 costToEnterInWei = (entranceFeeParsed * 10 ** 8 * 10 ** 18) /
+            uint256(price);
+        return costToEnterInWei;
     }
 
     function enter() public payable checkOpened {
@@ -122,10 +139,10 @@ contract Lottery is VRFConsumerBaseV2, Ownable {
         s_maxParticpantsLimit = _newLimit;
     }
 
-    function fulfillRandomWords(uint256, uint256[] memory _randomWords)
-        internal
-        override
-    {
+    function fulfillRandomWords(
+        uint256,
+        uint256[] memory _randomWords
+    ) internal override {
         // * this function is for the callback from the VRF node. Can be called only from the VRF node. (check docs for more info. (request response cycle))
         require(s_lotteryState == LotteryState.SELECTING_WINNER);
         require(_randomWords[0] > 0);
@@ -138,7 +155,7 @@ contract Lottery is VRFConsumerBaseV2, Ownable {
 
         // This will transfer 20 percent to the owner.
         (bool sucessOwnerCut, ) = payable(owner()).call{
-            value: address(this).balance * 20 / 100
+            value: (address(this).balance * 20) / 100
         }("");
         if (!sucessOwnerCut) revert Lottery__TransferFailed();
         (bool success, ) = s_recentWinner.call{value: address(this).balance}(
